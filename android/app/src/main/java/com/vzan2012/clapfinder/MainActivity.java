d45186c8ccf9c7e672e51714517c9a80c1ca4253
Deepak Guptha <vzan2012@gmail.com>
2026-07-12 19:59:44 -0400

    fix: update java permissions

--- android/app/src/main/java/com/vzan2012/clapfinder/MainActivity.java
+++ android/app/src/main/java/com/vzan2012/clapfinder/MainActivity.java
@@ -1,5 +1,36 @@
 package com.vzan2012.clapfinder;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Wait for the Capacitor webview bridge to initialize
+        this.bridge.getWebView().post(new Runnable() {
+            @Override
+            public void run() {
+                WebView webView = bridge.getWebView();
+                
+                // Intercept web runtime hardware requests and grant them natively
+                webView.setWebChromeClient(new WebChromeClient() {
+                    @Override
+                    public void onPermissionRequest(final PermissionRequest request) {
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Explicitly grants the WebView wrapper permission to grab the microphone stream
+                                request.grant(request.getResources());
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+}
